09b186f432993e0ea279b3c10c41dfb35bf23f31
lexlex47 <lixianglex0727@live.com>
2021-02-01 22:05:42 +1100

    first commit

---
+++ lib/fresho.rb
@@ -0,0 +1,6 @@
+require "fresho/version"
+
+module Fresho
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/fresho_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Fresho do
+  it "has a version number" do
+    expect(Fresho::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
